56635e3154b35e7cf85a9886bed197d9d4e775ac
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-02-02 17:49:46 +0800

    feat： ActionBar相關設置

--- app/src/main/java/com/hyak4j/cb/tarvel/taipei/MainActivity.kt
+++ app/src/main/java/com/hyak4j/cb/tarvel/taipei/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hyak4j.cb.tarvel.taipei
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Actionbar
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.action_bar)
     }
 }
