8410fd4c903f0c0de13e400dee782596a6b47f95
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-02-08 15:38:34 +0800

    feat：最新消息url載入webview顯示

--- app/src/main/java/com/hyak4j/cb/tarvel/taipei/ui/view/news/NewsFragment.kt
+++ app/src/main/java/com/hyak4j/cb/tarvel/taipei/ui/view/news/NewsFragment.kt
@@ -4,26 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.hyak4j.cb.tarvel.taipei.R
+import com.hyak4j.cb.tarvel.taipei.databinding.FragmentNewsBinding
 
-class NewsFragment : Fragment() {
+class NewsFragment(private val url: String) : Fragment() {
+    private lateinit var binding: FragmentNewsBinding
 
     companion object {
-        fun newInstance() = NewsFragment()
+        fun newInstance(url: String) = NewsFragment(url)
     }
-
-    private lateinit var viewModel: NewsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_news, container, false)
+    ): View {
+        binding = FragmentNewsBinding.inflate(inflater, container, false)
+
+        val actionBar = (activity as AppCompatActivity?)?.supportActionBar
+        // ActionBar Title更換為最新消息
+        val customView = actionBar?.customView
+        val textViewTitle = customView?.findViewById<TextView>(R.id.title)
+        textViewTitle?.text = resources.getString(R.string.news)
+
+        // webview相關設定
+        val newsWebView = binding.webviewNews
+        val webSettings = newsWebView.settings
+        webSettings.javaScriptEnabled = true
+        newsWebView.loadUrl(url)
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
     }
-
 }
--- app/src/main/java/com/hyak4j/cb/tarvel/taipei/ui/viewmodel/news/NewsAdapter.kt
+++ app/src/main/java/com/hyak4j/cb/tarvel/taipei/ui/viewmodel/news/NewsAdapter.kt
@@ -2,9 +2,12 @@ package com.hyak4j.cb.tarvel.taipei.ui.viewmodel.news
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.hyak4j.cb.tarvel.taipei.R
 import com.hyak4j.cb.tarvel.taipei.databinding.ItemNewsBinding
 import com.hyak4j.cb.tarvel.taipei.model.news.Data
+import com.hyak4j.cb.tarvel.taipei.ui.view.news.NewsFragment
 
 class NewsAdapter(private var _news: List<Data>) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
@@ -26,10 +29,16 @@ class NewsAdapter(private var _news: List<Data>) :
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.binding.root.setOnClickListener {
+            val newFragment = NewsFragment.newInstance(_news.get(position).url)
+            val transaction = (holder.itemView.context as AppCompatActivity).supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_container, newFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
         holder.binding.txtNewsTitle.text = _news.get(position).title
         // 發現消息敘述中有換行及空白符號，呈現前進行濾除
         holder.binding.txtNewsDescription.text = _news.get(position).description
                                                       .replace("[\\r\\n\\s]*&nbsp;".toRegex(), "").trim()
     }
-
 }
